cc5755e730cfc795c1fd989545c0a6911c5cd3fe
Sascha Teske <sascha.teske@gmail.com>
2017-01-25 08:54:33 +0100

    First test

---
+++ bin/gitbak.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -ex
+
+exec 1<>/tmp/gitbak.sh.log
+exec 2<>/tmp/gitbak.sh.err
+
+FULLNAME=$(readlink -f $1)
+DIRNAME=$(dirname $FULLNAME)
+BAKDIR=$2
+TARGETNAME=$BAKDIR$FULLNAME
+TARGETDIR=$BAKDIR$DIRNAME
+
+[[ -e $BAKDIR ]] || mkdir -p $BAKDIR
+cd $BAKDIR
+[[ -e $BAKDIR/.git ]] || git init
+
+
+mkdir -p $TARGETDIR
+cp $FULLNAME $TARGETNAME
+git add .
+git commit -m "Vim backup"
+
